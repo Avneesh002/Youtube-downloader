1b4ea341dff5bb784027401061b55e2af59fe258
Avneesh002 <Avneeshgrover8@gmail.com>
2023-02-07 03:19:34 +0530

    adding files

---
+++ src/components/Search.jsx
@@ -0,0 +1,81 @@
+import { Button, Flex, Input, Stack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { SearchIcon } from "@chakra-ui/icons";
+import getYouTubeID from "get-youtube-id";
+
+let vals = JSON.parse(localStorage.getItem("val"));
+
+const Search = ({
+  videos,
+  setVideos,
+  data,
+  setData,
+  loader,
+  setLoader,
+  setState,
+  state,
+}) => {
+  const [id, setId] = useState("");
+  const [url, setUrl] = useState("");
+  const [click, setClick] = useState(false);
+
+  const handleChange = (e) => {
+    setUrl(e.target.value);
+    setClick(!click);
+  };
+
+  useEffect(() => {
+    let vdoId = getYouTubeID(`${url}`);
+    setId(vdoId);
+  }, [click]);
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "65d9dfef2dmshbdd543f9b7a8761p1c27afjsnd7b785242d5c",
+      "X-RapidAPI-Host": "youtube-video-download-info.p.rapidapi.com",
+    },
+  };
+
+  const handleBtn = (options, id) => {
+    setState(true);
+    setLoader(true);
+    fetch(
+      `https://youtube-video-download-info.p.rapidapi.com/dl?id=${id}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        localStorage.setItem("val", JSON.stringify(response));
+      });
+
+    setLoader(false);
+  };
+  // console.log(vals);
+  setData(vals);
+  setVideos(vals.link);
+  return (
+    <>
+      <Stack pb={"50px"}>
+        <Flex>
+          <Input
+            onChange={(e) => handleChange(e)}
+            borderColor={"blue.900"}
+            focusBorderColor="blue.400"
+            placeholder="Enter URL here"
+            type={"url"}
+          />
+          <Button
+            variant={"solid"}
+            onClick={() => handleBtn(options, id)}
+            bg={"rgb(49, 113, 153)"}
+          >
+            <SearchIcon color={"black"} fontWeight="500" />
+          </Button>
+        </Flex>
+      </Stack>
+    </>
+  );
+};
+
+export default Search;
